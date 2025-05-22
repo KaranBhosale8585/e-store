675b4d6afdf221ff84939c7c6dee10eb4d9f1ee2
Karan <kb466444@gmail.com>
2025-05-22 14:37:17 +0530

    UI Changes

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,7 @@ export default function Home() {
 
         {/* Product Grid */}
         <main className="flex-1 p-4 sm:p-6 lg:p-8 mt-4 md:mt-0">
-          <div className="grid grid-cols-1 text-black sm:grid-cols-2 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 text-black md:grid-cols-2 lg:grid-cols-3 gap-6">
             {filteredProducts.length > 0 ? (
               filteredProducts.map((product) => (
                 <div
